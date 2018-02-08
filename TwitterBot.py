5d0e688999a424ed270669e4a1b680791301ce33
ksegu <kishansegu@yahoo.com>
2018-02-08 16:18:25 -0500

    Update TwitterBot.py

--- TwitterBot.py
+++ TwitterBot.py
@@ -4,24 +4,20 @@ import tweepy
 import secrets
 
 
-
-
-
     #get authorization
 auth = tweepy.OAuthHandler(secrets.consKey, secrets.consSecret)
 auth.set_access_token(secrets.accessTokn, secrets.secretAccessTokn)
 bot = tweepy.API(auth)
-joke = "My boss yelled at me the other day, You’ve got to be the worst train driver in history. How many trains did you derail last year?"
-#bot.update_status(joke)
+opportunity = "Anyone looking for volunteer opprtunities in the area"
+bot.update_status(opportunity)
 
 search_text = "#communityservice"
 search_number = 2
-search_result = bot.search("trump")
-#search_result = bot.search(search_text,rpp = search_number)
+search_result = bot.search(search_text,rpp = search_number)
 
 for i in search_result:
-    status = bot.get_status(i.id)
-    print(status.text)
+    opportunity = bot.get_status(i.id)
+    print(opportunity.text)
 
 
 
